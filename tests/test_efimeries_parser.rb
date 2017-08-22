277e6391d9049caf84d0eca620f5cf5584c37813
Charitakis Yannis <github@ych.gr>
2017-08-22 15:21:29 +0300

    Better test

--- tests/test_efimeries_parser.rb
+++ tests/test_efimeries_parser.rb
@@ -1,4 +1,14 @@
 load "../efimeries_parser.rb"
 
 ep = EfimeriesParser.new "efimeries.ods"
-puts ep.parse
+result = ep.parse 
+
+if result=="OK"
+  puts "All OK"
+  ep.each_teacher do |t|
+    puts t
+  end
+else
+  puts "Something happed!"
+  print result
+end
