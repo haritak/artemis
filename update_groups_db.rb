9745d474eb7b3cfc4264c560f7d6f5a0f6a9afa1
Charitakis Yannis <github@ych.gr>
2017-08-22 15:22:01 +0300

    Completed code to update emails.db and whosin.db

--- update_groups_db.rb
+++ update_groups_db.rb
@@ -1,10 +1,101 @@
+require "sqlite3"
+
+load "efimeries_parser.rb"
+
 class UpdateScheduleGroupsAction < ScheduleBaseAction
+
+  def initialize
+    FileUtils.touch('emails.db')
+    db = SQLite3::Database.open "emails.db"
+    db.execute "CREATE TABLE IF NOT EXISTS " +
+      "teachers(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE, email TEXT)"
+    db.close
+
+
+    FileUtils.touch('whosin.db')
+    db = SQLite3::Database.open "whosin.db"
+    db.execute "CREATE TABLE IF NOT EXISTS "+
+      "teachers(id INTEGER PRIMARY KEY AUTOINCREMENT, timetables_name TEXT UNIQUE, using_groups TINYINT)"
+    db.close
+
+    begin
+      FileUtils.ln_s('../whosin.db', 'helper/whosin.db')
+    rescue
+    end
+  end
+
   def describe
     "Updating schedule related databases"
   end
+
   def process(m)
-    super
+    first_pass = super
+    return first_pass if first_pass!=CONTINUE
     return CONTINUE if not @fromSchedulers
-    return CONTINUE if not @aboutSchedule or not @aboutEfimeries
+    return CONTINUE if not @aboutSchedule and not @aboutEfimeries
+
+    # at this point:
+    # the email has been checked against attachments
+    # has been sent by the schedulers
+    # and it is about the schedule or efimeries
+    #
+    if @aboutSchedule
+      return process_schedule(m)
+    else
+      return process_efimeries(m)
+    end
+  end
+
+  private
+
+  def process_schedule(m)
+    xls_idx = @attachments.index( EXCEL_FILENAME )
+    filename = saveLocal( @attachments_contents[xls_idx] )
+
+    %x{ cd helper && php update_whosin.php #{"../"+filename} }
+
+    return CONTINUE
+  end
+
+  def process_efimeries(m)
+    ef_idx = @attachments.index( EFIMERIES_FILENAME )
+    filename = saveLocal( @attachments_contents[ef_idx] )
+
+    parser = EfimeriesParser.new filename
+    parser.beSilent
+
+    result = parser.parse
+    if result != "OK"
+      m = "Error while parsing: " + result
+      Artemis::send_warning_email(m, ['charitakis.ioannis@gmail.com'], warning) #TODO should inform everyone SCHEDULERS
+      return STOP_PROCESSING
+    end
+
+    db = SQLite3::Database.open "emails.db"
+    parser.each_teacher do |t|
+      t.strip!
+      sql = "SELECT * FROM teachers WHERE name LIKE '%#{t}%' "
+      stm = db.prepare sql
+      rs = stm.execute
+      if not row=rs.next
+        db.execute "INSERT INTO teachers VALUES(NULL, '#{t}','')"
+      end
+      stm.close
+    end
+    db.close
+
+    return CONTINUE
+  end
+
+
+  def saveLocal(attachment)
+    filename = attachment.filename
+    FileUtils.mkdir("tmp") if not File.exist?("tmp/")
+    FileUtils.rm("tmp/#{filename}") if File.exist?("tmp/#{filename}")
+    filename = "tmp/#{filename}"
+    File.open(filename, "w+b", 0644) do |f| 
+      f.write attachment.body.decoded
+    end
+    return filename
   end
 end
