2182a38b520764a8f27792481799eac6f6460a81
Charitakis Yannis <github@ych.gr>
2017-08-25 09:36:29 +0300

    Bug! Changed recipients of sensitive emails

--- sensitive_list.rb
+++ sensitive_list.rb
@@ -31,7 +31,7 @@ class MonitorSensitiveList < Dummy_Action
     msg="<p><em>Κάποιο μήνυμα περιμένει στην λίστα των ευαίσθητων</em></p>"+
       "<p>Παρακαλώ ενημερώστε τον διευθυντή/υποδιευθυντή.</p>"+
       "<p><small>Sms balance: #{@balance}.</small></p>"
-    Artemis::send_email(m, SCHEDULERS, msg)
+    Artemis::send_email(m, SENSITIVE_EMAIL_RECIPIENTS, msg)
 
     return STOP_PROCESSING
   end
