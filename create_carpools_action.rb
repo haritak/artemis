fff6f0eb133abb06ef13e4162b32c58ceea7683b
Charitakis Yannis <github@ych.gr>
2017-11-09 18:27:08 +0200

    GUI changes

--- create_carpools_action.rb
+++ create_carpools_action.rb
@@ -58,7 +58,9 @@ class CreateCarPoolsAction < ScheduleBaseAction
       "Τα αρχεία των groups δημιουργήθηκαν επιτυχώς.</em></p>"+
       "<p>Μπορείτε να ρυθμίσετε ποιοί/ες συμμετέχουν στα groups"+
       " χρησιμοποιώντας <br/>"+
-      " <a href='http://srv-1tee-moiron.ira.sch.gr:4567'>αυτό το link</a>.</p>"
+      " <a href='http://srv-1tee-moiron.ira.sch.gr:4567'>αυτό το link</a>.<br/>" +
+      "Σε περίπτωση που κάνετε αλλαγές στην σύνθεση των group, στείλτε μου ένα email<br/>" +
+      "με θέμα : regroup</p>"
     Artemis::send_email(m, SCHEDULERS, msg, results)
 
     return CONTINUE
