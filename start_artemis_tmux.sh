3573b20314872c5461fc44eaebde1a3f4e9cc628
Charitakis Yannis <github@ych.gr>
2018-09-21 05:40:01 +0300

    Small UI change

--- start_artemis_tmux.sh
+++ start_artemis_tmux.sh
@@ -4,7 +4,7 @@ tmux send-keys -t artemis 'cd /home/administrator/artemis/stand_alone' C-m
 tmux send-keys -t artemis 'ruby sinatra_whosin.rb' C-m
 tmux new-window -t artemis
 tmux send-keys -t artemis 'cd /home/administrator/artemis' C-m
-tmux send-keys -t artemis 'while true; do ruby artemis.rb ; done' C-m
+tmux send-keys -t artemis 'while true; do ruby artemis.rb ; echo "Back to shell."; done' C-m
 tmux new-window -t artemis
 tmux send-keys -t artemis 'cd /home/administrator/artemis/stand_alone' C-m
 tmux send-keys -t artemis 'ruby processEmails.rb' C-m
