17545c8ec3c7dd0abaa6aec7370b438622d244d9
administrator@srv-1tee-moiron <administrator@srv-1tee-moiron>
2019-12-09 18:25:41 +0200

    Simplified start_artemis script

--- start_artemis_tmux.sh
+++ start_artemis_tmux.sh
@@ -7,7 +7,4 @@ tmux send-keys -t artemis 'cd /home/administrator/artemis' C-m
 tmux send-keys -t artemis 'while true; do ruby artemis.rb ; echo "Back to shell."; done' C-m
 tmux new-window -t artemis
 tmux send-keys -t artemis 'cd /home/administrator/artemis/stand_alone' C-m
-tmux send-keys -t artemis 'ruby processEmails.rb' C-m
-tmux new-window -t artemis
-tmux send-keys -t artemis 'cd /home/administrator/artemis/stand_alone' C-m
 tmux send-keys -t artemis 'ruby sinatra_wake_me_up.rb' C-m
